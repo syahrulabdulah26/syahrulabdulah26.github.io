0eaea25a8c8927bc406b60f805b7e99e880ec103
syahrulabdulah26 <122715747+syahrulabdulah26@users.noreply.github.com>
2023-01-15 13:42:26 +0700

    Add files via upload

---
+++ form-php-ke-email/README.txt
@@ -0,0 +1,14 @@
+untuk keterangan script ada di sini:
+http://www.bayuajie.com/blog/2017/05/26/membuat-form-php-ke-email/
+
+script ini dapat dimodifikasi untuk berbagai keperluan seperti:
+- form pemesanan pada sebuah toko online
+- form komplain
+- form support clien
+- form konfirmasi
+dan sebagainya 
+
+untuk modifikasi sesuai dengan kebutuhan anda, silahkan hubungi saya
+Whatsapp: 0813 9024 1937
+
+soal fee bisa dibicarakan, sob .. hahahaa :D
---
+++ form-php-ke-email/kulit.php
@@ -0,0 +1,33 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+  </head>
+  <body>
+  <br/><br/><br/>
+ <div class="row">
+  <div class="col-md-4"></div>
+  <div class="col-md-4">  
+	<form action='kirim.php' method='post'>
+	<div class="form-group">
+		<label >Nama</label>
+		<input type="text" class="form-control" name="nama">
+	</div>
+	<div class="form-group">
+		<label >Nomer HP</label>
+		<input type="text" class="form-control" name="hp">
+	</div>
+    <div class="form-group">
+		<label >Alamat</label>
+		<input type="text" class="form-control" name="alamat">
+	</div>	
+	<div class="form-group">
+		<label >Email</label>
+		<input type="text" class="form-control" name="usermail">
+	</div>		
+		<button type='Submit'>Submit</button>
+	</form>
+    </div>
+	</div>
+  </body>
+</html>
