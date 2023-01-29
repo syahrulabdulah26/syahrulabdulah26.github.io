93dd573345b2b0fbde052e3dd462b0f87ae2c637
syahrulabdulah26 <122715747+syahrulabdulah26@users.noreply.github.com>
2023-01-29 07:10:17 +0700

    Delete form-php-ke-email directory

--- form-php-ke-email/PHPmailer/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-require 'Send_Mail.php';
-$to = "srinivas.tamada@gmail.com";
-$subject = "Test Mail Subject";
-$body = "Hi<br/>Test Mail<br/>Amazon SES"; // HTML  tags
-Send_Mail($to,$subject,$body);
-?>
